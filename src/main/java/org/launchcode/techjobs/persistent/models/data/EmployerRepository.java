824ebedc9f447cab9237da0b499fe6edd0c6cbac
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-11-08 09:32:55 -0500

    Task3

--- src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
@@ -1,8 +1,10 @@
 package org.launchcode.techjobs.persistent.models.data;
 
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Job;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployerRepository extends CrudRepository {
+public interface EmployerRepository extends CrudRepository <Employer, Integer>{
 }
